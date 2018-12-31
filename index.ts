347f7a72adfc9bb4f478df93fe137b63c05b5c48
JosephDeffid <luks.colombia89@gmail.com>
2018-12-31 15:18:41 -0500

    sección 4

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import router  from './routes/router';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const server = new Server();
+const server = Server.instance;
 
 //BodyParser
 server.app.use( bodyParser.urlencoded({ extended: true}));
@@ -17,6 +17,6 @@ server.app.use('/', router)
 
 server.start(()=>{
 
-    console.log( `Servidor corriendo en el puerto ${ server.port}` );
+    console.log( `Servidor corriendo en el puerto ${ server.port }` );
 
 });
